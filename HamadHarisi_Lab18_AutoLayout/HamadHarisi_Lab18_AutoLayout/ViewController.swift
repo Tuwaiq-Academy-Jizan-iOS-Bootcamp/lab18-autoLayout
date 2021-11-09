351c336e353f6fe9449636cb545da78f28d28c39
hamad <7amad7arisi@gmail.com>
2021-11-09 23:30:53 +0300

    HamadHarisi_AutoLayOut_version_1

---
+++ HamadHarisi_Lab18_AutoLayout/HamadHarisi_Lab18_AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HamadHarisi_Lab18_AutoLayout
+//
+//  Created by حمد الحريصي on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
