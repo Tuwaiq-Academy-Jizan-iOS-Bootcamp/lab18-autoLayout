5116b828934cc835e30b4af1e1c083d55d39c9d9
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-11-09 21:27:05 +0300

    HananSomily_lab18-autoLayout

---
+++ HananSomily_lab18-autoLayout/HananSomily_lab18-autoLayoutTests/HananSomily_lab18_autoLayoutTests.swift
@@ -0,0 +1,33 @@
+//
+//  HananSomily_lab18_autoLayoutTests.swift
+//  HananSomily_lab18-autoLayoutTests
+//
+//  Created by Hanan Somily on 09/11/2021.
+//
+
+import XCTest
+@testable import HananSomily_lab18_autoLayout
+
+class HananSomily_lab18_autoLayoutTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
