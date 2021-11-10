a26d2a19eed18d1e7cc66f49bd19f025b9e361ef
YOUSEF ALMALKI <mmrdas600@gmail.com>
2021-11-10 16:35:34 +0300

    done

---
+++ lyout/lyout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lyout
+//
+//  Created by يوسف جابر المالكي on 04/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
