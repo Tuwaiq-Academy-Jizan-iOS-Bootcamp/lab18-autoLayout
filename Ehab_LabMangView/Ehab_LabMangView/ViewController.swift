a2e653acb7b88a69174025c39f7408c90b0c8717
Ehab hakami <ehab7k@gmail.com>
2021-11-10 12:03:50 +0300

    ehabLab18

---
+++ Ehab_LabMangView/Ehab_LabMangView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ehab_LabMangView
+//
+//  Created by Ehab Hakami on 04/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
