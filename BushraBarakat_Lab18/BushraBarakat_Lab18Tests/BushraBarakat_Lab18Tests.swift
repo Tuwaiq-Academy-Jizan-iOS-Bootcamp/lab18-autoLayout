09fda5d6a2bc64d188857d5bc9d3d51fb9342ba5
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-11-10 10:33:54 +0300

    Bushra Lab18

---
+++ BushraBarakat_Lab18/BushraBarakat_Lab18Tests/BushraBarakat_Lab18Tests.swift
@@ -0,0 +1,33 @@
+//
+//  BushraBarakat_Lab18Tests.swift
+//  BushraBarakat_Lab18Tests
+//
+//  Created by Bushra Barakat on 05/04/1443 AH.
+//
+
+import XCTest
+@testable import BushraBarakat_Lab18
+
+class BushraBarakat_Lab18Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
