16d3d12b9b1f61a0c51a2997914602af803553b3
DAHMAJABER <dahma.alwani.jiz.s@tuwaiq.edu.sa>
2021-11-12 20:35:24 +0300

    lab18

---
+++ lab18 dahma/lab18 dahma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab18 dahma
+//
+//  Created by dahma alwani on 08/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
